343fa50a3e67bd2e13c17a70a89b816899b45422
Isaac Heist <isaacheist@gmail.com>
2019-07-25 16:50:40 -0700

    expanded rate to 3 and added new title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container text-center">
-    <h1 class="mb-3">Text To Speech</h1>
+    <h1 class="mb-3">Read Text Aloud</h1>
     <div class="row">
       <div class="col-lg-6 mx-auto">
         <form>
@@ -20,7 +20,7 @@
               id="rate"
               class="custom-range"
               min="0.5"
-              max="2"
+              max="3"
               :value="rate"
               v-on:change="handleChange"
               step="0.1"
@@ -44,13 +44,19 @@
             <select name id="voice-select" class="form-control form-control-lg"></select>
           </div>
           <button v-on:click.prevent="speak" class="btn btn-success btn-lg btn-block">Speak</button>
-          <button v-on:click.prevent="togglePause"
-                  class="btn btn-info btn-lg btn-block"
+          <button
+            v-on:click.prevent="togglePause"
+            class="btn btn-info btn-lg btn-block"
           >{{pauseStatus}}</button>
           <button v-on:click.prevent="stop" class="btn btn-danger btn-lg btn-block">Stop</button>
           <label for="toggleRepeat" class="toggle-repeat mt-1">
             Repeat
-            <input type="checkbox" id="toggleRepeat" class="toggle-repeat_input" v-model="repeat"/>
+            <input
+              type="checkbox"
+              id="toggleRepeat"
+              class="toggle-repeat_input"
+              v-model="repeat"
+            >
           </label>
         </form>
       </div>
@@ -60,20 +66,20 @@
 
 <script>
 export default {
-  name: 'app',
+  name: "app",
   data: () => ({
     synth: window.speechSynthesis,
     voices: [],
     rate: 1,
     pitch: 1,
-    pauseStatus: 'Pause',
+    pauseStatus: "Pause",
     repeat: false,
-    manualStop: false,
+    manualStop: false
   }),
   methods: {
     handleChange(e) {
       const { value, id } = e.target;
-      if (id === 'rate') {
+      if (id === "rate") {
         this.rate = value;
         return null;
       }
@@ -83,44 +89,42 @@ export default {
     getVoices() {
       this.voices = this.synth.getVoices();
       if (this.voices.length > 0) {
-        this.voices.map((voice) => {
-          const option = document.createElement('option');
+        this.voices.map(voice => {
+          const option = document.createElement("option");
 
           option.textContent = `${voice.name}(${voice.lang})`;
-          option.setAttribute('data-lang', voice.lang);
-          option.setAttribute('data-name', voice.name);
+          option.setAttribute("data-lang", voice.lang);
+          option.setAttribute("data-name", voice.name);
           this.voiceSelect.appendChild(option);
           return null;
         });
       }
     },
     speak() {
-      const {
-        synth, textInput, voices, voiceSelect, repeat,
-      } = this;
+      const { synth, textInput, voices, voiceSelect, repeat } = this;
       if (synth.speaking) {
-        console.error('Already speaking...');
+        console.error("Already speaking...");
         return;
       }
-      if (textInput.value !== '') {
+      if (textInput.value !== "") {
         const speakText = new SpeechSynthesisUtterance(textInput.value);
 
         speakText.onend = () => {
           if (repeat) {
             this.speak();
           }
-          console.log('Done speaking...');
+          console.log("Done speaking...");
         };
 
-        speakText.onerror = (err) => {
-          console.log('Something went wrong speaking:\n', err);
+        speakText.onerror = err => {
+          console.log("Something went wrong speaking:\n", err);
         };
 
         const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
-          'data-name',
+          "data-name"
         );
 
-        voices.map((voice) => {
+        voices.map(voice => {
           if (voice.name === selectedVoice) {
             speakText.voice = voice;
           }
@@ -130,12 +134,12 @@ export default {
         speakText.pitch = this.pitch;
 
         synth.speak(speakText);
-        console.log('Speaking...');
+        console.log("Speaking...");
       }
     },
     stop() {
       this.repeat = false;
-      this.pauseStatus = 'Pause';
+      this.pauseStatus = "Pause";
       this.synth.cancel();
     },
     togglePause() {
@@ -143,24 +147,24 @@ export default {
       if (!synth.speaking && !synth.paused) {
         return;
       }
-      if (this.pauseStatus === 'Pause') {
+      if (this.pauseStatus === "Pause") {
         synth.pause();
-        this.pauseStatus = 'Resume';
+        this.pauseStatus = "Resume";
         return;
       }
       synth.resume();
-      this.pauseStatus = 'Pause';
-    },
+      this.pauseStatus = "Pause";
+    }
   },
   mounted() {
-    this.voiceSelect = document.getElementById('voice-select');
-    this.textInput = document.getElementById('text-input');
+    this.voiceSelect = document.getElementById("voice-select");
+    this.textInput = document.getElementById("text-input");
     this.getVoices();
 
     if (this.synth.onvoiceschanged !== undefined) {
       this.synth.onvoiceschanged = this.getVoices;
     }
-  },
+  }
 };
 </script>
 
